1c81e16d90cffbfc12e39796cfdf254195fd6568
monaeman <mona.eman2018@gmail.com>
2023-06-23 10:29:34 -0400

    added codes

--- script.js
+++ script.js
@@ -20,15 +20,18 @@ const colors = {
 
 const fetchPokemon = async() => {
     for (let i = 1; i <= pokemon_count; i++){
-        console.log(i)
+        //console.log(i)
         await getPokemon(i);
 
     }
 }
 
 const getPokemon = async (id) => {
-    const url = ``
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json();
+    console.log(data)
 }
 
-//fetchPokemon();
+fetchPokemon();
 
